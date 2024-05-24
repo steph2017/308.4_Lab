3166dfe5d23f186c9627ccd8e39674f3e2324ed6
StephOg <stephanieogwo@Stephanies-MBP.lan>
2024-05-24 16:16:44 -0400

    2nd draft of pt .3, pt.2 was already complete

--- 308.4Lab.js
+++ 308.4Lab.js
@@ -43,7 +43,7 @@ Cache this two-dimensional array in a variable for later use.
 // MAybe if i do a nested loop, i could solve for unlimited number of rows...
 // Actually - it will be much easier to use slice:
 
-const numOfCols = cv.indexOf("\n") + 1; // Don't know yet if its more important to + 1 at this point or leave be. UPDATE: need the + 1
+const numOfCols = parsedWords[0].length; // Don't know yet if its more important to + 1 at this point or leave be. UPDATE: need the + 1
 
 // I can calculate start/stop points of each based on loop counter. Making the new array:
 // UPDATE: I scrapped all the code - I updated code in part 1 to satisfy requirements in part 2. Reposting below:
@@ -69,20 +69,23 @@ Since the heading for each column will be stored in the object keys, you do not 
 // So i will need to loop through this, adding keys as i go.
 
 
-const cvObject = {};
+// const cvObjectArr = [];
+// const cvObjectRow = {};
 
-//First I'll remove the header array from the 2d array:
-const objectKeys = parsedWords.splice(0, 1);
+// //First I'll remove the header array from the 2d array:
+// const objectKeys = parsedWords.splice(0, 1);
 
-// Then ill go ahead and make it lowercase now:
-const objectKeyslc = object
-
-// Then I'll iterate thru numOfCols to create each key:
-for (i = 0; i < numOfCols; i++) {
-    cvObject[([i]).toLowerCase]: "";
-}
-color: "blue",
-    hp: 4000,
-        year: 1989,
-            "active registration": true
-    }
+// // Then ill go ahead and make it lowercase now:
+// // const objectKeyslc = objectKeys.map((key) => key.toLowerCase());
+// let i; //debug visibility
+// let j;
+// // Then I'll iterate thru numOfCols to create each key:
+// for (i = 0; i < numOfCols; i++) {
+//     cvObjectRow[objectKeys[i]];
+// }
+// for (j = 0; j < parsedWords.length; j++) {  // Then within each key, add in the corresponding values for each row.
+//     for (i = 0; i < numOfCols; i++) {
+//         cvObjectRow[objectKeys[i]] = parsedWords[j][i];
+//     }
+//     cvObjectArr.push(cvObjectRow);
+// }
